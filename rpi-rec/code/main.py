f5b698f5696f08721794a28f6dc35da77d2fce51
WillUniRal <22300044@bucks.ac.uk>
2026-05-17 14:58:53 +0100

    FixedPiPerformance

--- rpi-rec/code/main.py
+++ rpi-rec/code/main.py
@@ -19,25 +19,25 @@ piano.strip.begin()
 
 
 while True:
+
+    header = ser.read(header_len)
+    if len(header) != header_len: continue
+
+    method, msg_len = struct.unpack('B'*header_len, header)
+
+    while ser.in_waiting < msg_len : pass
+    data = ser.read(msg_len)
+
+    if method == Method.ON :
+        value, r, g, b = struct.unpack('B'*msg_len, data)
+        piano.turnNoteOn(value, r,g,b)
+    else :
+        value = data[0]
+        piano.turnNoteOff(value)
+        #print(f"value :{value} (off)")
+
+    piano.turnOnBuffers()
     
-    if ser.in_waiting >= header_len:
-
-        header = ser.read(header_len)
-        method, msg_len = struct.unpack('B'*header_len, header)
-
-        while ser.in_waiting < msg_len : pass
-        data = ser.read(msg_len)
-
-        if method == Method.ON :
-            value, r, g, b = struct.unpack('B'*msg_len, data)
-            piano.turnNoteOn(value, r,g,b)
-        else :
-            value = data[0]
-            piano.turnNoteOff(value)
-            #print(f"value :{value} (off)")
-
-        piano.turnOnBuffers()
-        
-        piano.strip.show()
+    piano.strip.show()
 
         
--- rpi-rec/commands.sh
+++ rpi-rec/commands.sh
@@ -6,3 +6,7 @@ rsync -avz --delete code/ dev@raspberrypi.local:~/MMMIDI-python
 # this will give the ipv6 required to connect to through ssh
 ping -I enp5s0 raspberrypi.local
 ping -I enp1s0 raspberrypi.local
+# setting up a service on the pi
+dev@raspberrypi:~/MMMIDI-python $ chmod +x SerialToLED.service
+dev@raspberrypi:~/MMMIDI-python $ sudo systemctl enable ~/MMMIDI-python/SerialToLED.service
+dev@raspberrypi:~/MMMIDI-python $ systemctl status SerialToLED.service
